0bfd4725bd3bd1b9002d4f3ed2c0653d8c9d923f
Lenarddz <48406626+l3narddz@users.noreply.github.com>
2022-11-18 17:35:29 +0800

    Add files via upload
    Nov 18, 2022

---
+++ rfidattendance/index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Door Lock System</title>
+  <link rel="icon" type="image/x-icon" href="Images/door-lock.png">
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="styles.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Ubuntu&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/1658d4fd0c.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <section id="title">
+    <!--navbar-->
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-dark navbar-margin">
+
+        <a class="navbar-brand" href="">Door Lock System</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#team">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#About-us">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#footer">Contact</a>
+            </li>
+            <div class="container">
+            <li class="nav-item">
+          <button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#loginModal">Login</button>
+          </div>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+    </div>
+    <!-- title-->
+    <div class="row">
+      <div class="col-lg-6">
+        <h1>Smart Security Starts at Your Front Door.</h1>
+      </div>
+      <div class="col-lg-6 img_title">
+        <img src="Images/294912423_767382284520561_3824341427877827275_n.png" alt="title_img" class="title-img">
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <!-- featured -->
+  <section id="features">
+
+    <div class="row">
+
+      <div class="feature-box col-lg-4">
+        <i class="icon fa-sharp fa-solid fa-circle-check fa-4x"></i>
+        <h3>Easy to use.</h3>
+        <p>So easy to use, even your dog could do it.</p>
+      </div>
+
+      <div class="feature-box col-lg-4">
+        <i class="icon fa-solid fa-bullseye fa-4x"></i>
+        <h3>Elite Client</h3>
+        <p>We have great client like STI</p>
+      </div>
+
+      <div class="feature-box col-lg-4">
+        <i class="icon fa-solid fa-heart fa-4x"></i>
+        <h3>Guaranteed to work.</h3>
+        <p>Keep track with your door, or your money back.</p>
+      </div>
+
+    </div>
+
+  </section>
+
+  <section id="team">
+    <h2>Our Team</h2>
+
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <img src="Images/camille_img.png" class="card-img-top" alt="Camille-img">
+          <div class="card-body">
+            <h5 class="card-title">Camille</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#">
+            <small class="text-muted">Contact Link</small>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="Images/sofia_img.png" class="card-img-top" alt="Sofia-img">
+          <div class="card-body">
+            <h5 class="card-title">Sofia</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <a href="mailto:sofiahiwatigabrenica@gmail.com">
+            <small class="text-muted">Contact Link</small>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="Images/joshuel_img.png" class="card-img-top" alt="Joshuel-img">
+          <div class="card-body">
+            <h5 class="card-title">Joshuel</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#">
+            <small class="text-muted">Contact Link</small>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="Images/lienard_img .png" class="card-img-top" alt="Lienard-img">
+          <div class="card-body">
+            <h5 class="card-title">Lienard</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <a href="mailto:lienarddiax@gmail.com">
+            <small class="text-muted">Contact Link</small>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- about us -->
+  <section id="About-us">
+    <h2 class="about">About</h2>
+    <p class="about-p">This is a Web-based door lock system that can be accessed with
+      the use of Radio Frequency Identification and can be controlled through a web application that intends to offer high security, easy access, and control.</p>
+  </section>
+
+  <!-- Call to Action -->
+  <section id="footer">
+
+    <div class="row footer-row">
+
+      <div class="col-lg-6">
+        <a href="#"><p class="footer-p">Door Lock System</p></a>
+        <p class="footer-p">Tel: 0919-581-9932</p>
+        <p class="footer-p">Email: DoorklockSystem@gmail.com</p>
+
+        <div class="row footer-div">
+          <div class="footer-icon col-lg-4">
+            <a href="mailto:lienarddiax@gmail.com">
+            <i class="fa-solid fa-envelope email_ fa-2x"></i>
+            </a>
+          </div>
+
+          <div class="footer-icon col-lg-4">
+            <a href="https://www.facebook.com/">
+            <i class="fa-brands fa-facebook fa-2x"></i>
+            </a>
+          </div>
+
+
+
+          <div class="footer-icon col-lg-4">
+            <a href="https://www.instagram.com/">
+            <i class="fa-brands fa-instagram fa-2x"></i>
+            </a>
+          </div>
+
+
+        </div>
+    </div>
+  </section>
+
+<!-- Login -->
+  <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-title text-center">
+          <h4>Login</h4>
+        </div>
+        <div class="d-flex flex-column text-center">
+          <form>
+            <div class="form-group">
+              <input type="email" class="form-control" id="email1"placeholder="Your email address...">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" id="password1" placeholder="Your password...">
+            </div>
+            <button type="button" class="btn btn-info btn-block btn-round">Login</button>
+          </form>
+
+          <div class="text-center text-muted delimiter">or use a social network</div>
+          <div class="d-flex justify-content-center social-buttons">
+            <button type="button" class="btn btn-secondary btn-round" data-toggle="tooltip" data-placement="top" title="Twitter">
+              <i class="fab fa-twitter"></i>
+            </button>
+            <button type="button" class="btn btn-secondary btn-round" data-toggle="tooltip" data-placement="top" title="Facebook">
+              <i class="fab fa-facebook"></i>
+            </button>
+            <button type="button" class="btn btn-secondary btn-round" data-toggle="tooltip" data-placement="top" title="Linkedin">
+              <i class="fab fa-linkedin"></i>
+            </button>
+        </div>
+      </div>
+    </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <div class="signup-section">Not a member yet? <a href="#a" class="text-info"> Sign Up</a>.</div>
+      </div>
+  </div>
+</div>
+  <!-- jQuery -->
+  <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
+  <!-- Popper JS -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
+  <!-- Bootstrap JS -->
+  <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
+
+
+</body>
+
+</html>
